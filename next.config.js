044de0145cdc6d466be3ffbcc98fac7dbf297314
Zaiko001 <94262658+Zaiko001@users.noreply.github.com>
2024-11-12 17:09:32 -0500

    Update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/bmoji_landing_page/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/bmoji_landing_page' : '',
   trailingSlash: true,
   reactStrictMode: true,
 }
