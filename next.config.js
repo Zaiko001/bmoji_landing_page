129cfbd00d6fb9e13288fdfcb21af6f301a26757
Zaiko001 <silvioricardomesa@gmail.com>
2024-11-12 17:37:45 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/bmoji_landing_page/' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/bmoji_landing_page' : '',
   trailingSlash: true,
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
